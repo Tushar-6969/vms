1d3bce49181a353e67bab47ad0d5ef7e07f30487
Tushar-6969 <144047695+Tushar-6969@users.noreply.github.com>
2025-08-12 19:53:57 +0530

    Update App.jsx

--- vms_ai_system/frontend/src/App.jsx
+++ vms_ai_system/frontend/src/App.jsx
@@ -39,7 +39,7 @@ export default function App() {
     }
   };
 
-  return (<div className="min-h-screen bg-gray-100 p-6" style={{ marginLeft: "500px" }}>
+  return (<div className="min-h-screen bg-gray-100 p-6" style={{ marginLeft: "300px" }}>
 
       <div className="max-w-4xl mx-auto">
         <h1 className="text-3xl font-bold mb-4 text-center">🎥 AI VMS Dashboard</h1>
